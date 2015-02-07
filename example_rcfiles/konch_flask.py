89cbe21ae7b4684f2fe77df278f9db39191a1745
Steven Loria <sloria1@gmail.com>
2015-02-07 01:48:00 -0500

    Update docs and changelog
    Add example usage of setup and teardown functions

--- example_rcfiles/konch_flask.py
+++ example_rcfiles/konch_flask.py
@@ -1,8 +1,14 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# vi: set ft=python :
 import flask
-
+from flask import Flask
 import konch
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello world'
 
 konch.config({
     'context': {
@@ -12,3 +18,13 @@ konch.config({
         'render_template': flask.render_template
     }
 })
+
+# Make sure we're in a request context so we can use
+# url_for
+ctx = app.test_request_context()
+def setup():
+    ctx.push()
+
+def teardown():
+    ctx.pop()
+
