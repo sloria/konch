80c3c08c69b8d47369a9af8016ea672785d519ca
Steven Loria <sloria1@gmail.com>
2024-01-10 10:23:08 -0500

    Support Python 3.12; prevent unnecessary builds

--- setup.py
+++ setup.py
@@ -97,6 +97,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: System :: Shells",
     ],
     py_modules=["konch", "docopt"],
